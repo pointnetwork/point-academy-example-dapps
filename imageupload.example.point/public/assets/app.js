e71d512b87c9af12ae1f9bedaa275a2366a2fe81
Darren Jensen <darren.jensen@gmail.com>
2022-10-29 16:32:57 +0700

    fix upload immediate display

--- imageupload.example.point/public/assets/app.js
+++ imageupload.example.point/public/assets/app.js
@@ -1,16 +1,16 @@
 async function uploadImage() {
-    const formData = new FormData()
+    const formData = new FormData();
     formData.append("myImage", fileupload.files[0]);
+
     const res = await window.point.storage.postFile(formData);
-    // save the image file id in the contract
-    await window.point.contract.call({contract: 'ImageStore', method: 'setStorageImage', params: [res.data]});
-    // now fetch the image and update the dom
-    await fetchImage()
+
+    await window.point.contract.call({contract: 'ImageStore', method: 'setStorageImage', params: [res.data]})
+    
+    document.getElementById('storageImg').setAttribute('src', `/_storage/${res.data}`)
 }
 
 async function fetchImage() {
-    // fetch the storage layer image id from the contract 
-    let {data: storageImageId} = await window.point.contract.call({contract: 'ImageStore', method: 'storageImage'});
-    // set the src attribute of the image to render the uploaded image (for visual testing)
+    let {data: storageImageId} = await window.point.contract.call({contract: "ImageStore", method: 'storageImage'});
+    
     document.getElementById('storageImg').setAttribute('src', `/_storage/${storageImageId}`)
 }
